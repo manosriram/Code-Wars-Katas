298407cdc6e7621e21445716a467f2e76102916f
Mano Sriram <mano.sriram0@gmail.com>
2019-09-11 22:32:08 +0530

    Change Indent Style VSCode Settings.

--- 8Kyu/exesOhs.cpp
+++ 8Kyu/exesOhs.cpp
@@ -1,24 +1,18 @@
 #include "../Template.hpp"
 
-bool exesOhs(string in)
-{
+bool exesOhs(string in) {
     int x = 0, o = 0;
-    for (int t = 0; t < in.length(); t++)
-    {
-        if (in[t] == 'x' || in[t] == 'X')
-        {
+    for (int t = 0; t < in.length(); t++) {
+        if (in[t] == 'x' || in[t] == 'X') {
             x++;
-        }
-        else if (in[t] == 'o' || in[t] == 'O')
-        {
+        } else if (in[t] == 'o' || in[t] == 'O') {
             o++;
         }
     }
     return (x == o);
 }
 
-int main()
-{
+int main() {
     cin.sync_with_stdio(false);
     cin.tie(NULL);
     ld T;
